665b12c4e072d60a47a576772262131a1c4c6664
Brian Wyrzykowski <brianwyrzykowski@gmail.com>
2012-11-21 16:20:37 -0600

    Initial commit of Rails Estore Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReplicaWyzard::Application.config.secret_token = 'e8b52bce74aa0b3a83ed58fccb40eec326ee7f09c99439e28690ea33c4aaaa6ce631904815d704f9b48b6fe94d32f4122396686368ded0b3b1c7aa64a4ca2e1e'
